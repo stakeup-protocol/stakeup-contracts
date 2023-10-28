64e7f638f0c26d13af26f551ca2e9dcbd3e9ac71
baileyspraggins <baileyspraggins@me.com>
2023-10-28 13:17:07 -0500

    Autostake feature added

--- src/interfaces/IBloomPool.sol
+++ src/interfaces/IBloomPool.sol
@@ -2,7 +2,23 @@
 pragma solidity 0.8.19;
 
 interface IBloomPool {
+    enum State {
+        Other,
+        Commit,
+        ReadyPreHoldSwap,
+        PendingPreHoldSwap,
+        Holding,
+        ReadyPostHoldSwap,
+        PendingPostHoldSwap,
+        EmergencyExit,
+        FinalWithdraw
+    }
+
     function depositLender(uint256 amount) external returns (uint256 newId);
 
+    function state() external view returns (State currentState);
+
     function withdrawLender(uint256 shares) external;
+
+    function COMMIT_PHASE_END() external view returns (uint256);
 }
