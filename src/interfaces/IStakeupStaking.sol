4f5201e78aec3e37e25c8fe2f4efa32e1dc5896a
baileyspraggins <baileyspraggins@me.com>
2023-11-07 13:03:43 -0600

    Initial StakeupStaking design

--- src/interfaces/IStakeupStaking.sol
+++ src/interfaces/IStakeupStaking.sol
@@ -3,22 +3,31 @@
 pragma solidity 0.8.19;
 
 interface IStakeupStaking {
+
+    // @notice Token amount is 0
+    error ZeroTokensStaked();
+
+    // @notice User has no current stake
+    error UserHasNoStaked();
+
+    // @notice User has no rewards to claim
+    error NoRewardsToClaim();
+
     // --- Events --
 
-    event stakeupAddressSet(address _stakeupAddress);
-    event stUSDAddressSet(address _stUSDAddress);
+    // event stakeupAddressSet(address _stakeupAddress);
+    // event stUSDAddressSet(address _stUSDAddress);
 
-    event StakeChanged(address indexed staker, uint256 newStake);
-    event StakingGainsWithdrawn(address indexed staker, uint256 STUSDGain);
-    event F_STUSDUpdated(uint256 _F_STUSD);
-    event totalStakeUpStaked(uint256 _totalStakeupStaked);
-    event StakerSnapshotsUpdated(address _staker, uint256 _F_STUSD);
+    // event StakeChanged(address indexed staker, uint256 newStake);
+    // event StakingGainsWithdrawn(address indexed staker, uint256 STUSDGain);
+    // event F_STUSDUpdated(uint256 _F_STUSD);
+    // event StakerSnapshotsUpdated(address _staker, uint256 _F_STUSD);
 
-    function stake(uint256 _StakeupAmount) external;
+    function stake(uint256 stakeupAmount) external;
 
-    function unstake(uint256 _StakeupAmount) external;
+    function unstake(uint256 stakeupAmount, uint256 harvestAmount) external;
 
-    function increaseF_STUSD(uint256 _STAKEUPFee) external;
+    function harvest() external;
 
-    function getPendingSTUSDGain(address _user) external view returns (uint256);
+    function harvest(uint256 amount) external;
 }
