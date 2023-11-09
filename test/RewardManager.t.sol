da7f0499e372098ee012845945669e32df8e1cc2
baileyspraggins <baileyspraggins@me.com>
2023-11-09 15:47:14 -0600

    Remove console2

--- test/RewardManager.t.sol
+++ test/RewardManager.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {LibRLP} from "solady/utils/LibRLP.sol";
-import "forge-std/console2.sol";
+
 import {RewardManager, IRewardManager} from "src/rewards/RewardManager.sol";
 import {ICurveGaugeDistributor} from "src/interfaces/ICurveGaugeDistributor.sol";
 
