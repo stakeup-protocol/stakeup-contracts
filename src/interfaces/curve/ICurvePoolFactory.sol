f75c6337f2220141a4776df9e50afc0dade1ad66
baileyspraggins <baileyspraggins@me.com>
2023-11-09 15:42:59 -0600

    Curve Reward Gauges

---
+++ src/interfaces/curve/ICurvePoolFactory.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+interface ICurvePoolFactory {
+
+    /**
+     * @notice Deploys a gauge for the given pool
+     * @param pool Address of the pool to deploy a gauge for
+     * @return Address of the deployed gauge
+     */
+    function deploy_gauge(address pool) external returns (address);
+}
---
+++ test/mock/Curve/MockCurveGauge.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ICurvePoolGauge} from "src/interfaces/curve/ICurvePoolGauge.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MockCurveGauge is ICurvePoolGauge {
+    address private _rewardToken;
+    address private _distributor;
+
+    function add_reward(
+        address reward_token,
+        address distributor
+    ) external override {
+        _rewardToken = reward_token;
+        _distributor = distributor;
+    }
+
+    function deposit_reward_token(
+        address reward_token,
+        uint256 amount
+    ) external override {
+        if (msg.sender != _distributor) revert("Invalid caller");
+        IERC20(reward_token).transferFrom(msg.sender, address(this), amount);
+    }
+}
