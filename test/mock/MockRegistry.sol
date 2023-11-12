74a80279b7dcd44edadc71233b38be6a6b9b93e8
baileyspraggins <baileyspraggins@me.com>
2023-11-11 19:03:16 -0600

    Adjust valuation calculations

--- test/mock/MockRegistry.sol
+++ test/mock/MockRegistry.sol
@@ -17,6 +17,8 @@ import {MockERC20} from "./MockERC20.sol";
 contract MockRegistry is IExchangeRateRegistry {
     address public pool;
     TokenInfo public tokenInfo;
+    address[] activeTokens;
+    uint256 exchangeRate;
 
     constructor(address _pool) {
         pool = _pool;
@@ -41,9 +43,23 @@ contract MockRegistry is IExchangeRateRegistry {
         view
         override
         returns (address[] memory)
-    {}
+    {
+        return activeTokens;
+    }
+
+    function setActiveTokens(address[] memory tokens) public {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            activeTokens.push(tokens[i]);
+        }
+    }
 
     function getExchangeRate(
-        address token
-    ) external view override returns (uint256) {}
+        address /*token*/
+    ) external view override returns (uint256) {
+        return exchangeRate;
+    }
+
+    function setExchangeRate(uint256 rate) public {
+        exchangeRate = rate;
+    }
 }
