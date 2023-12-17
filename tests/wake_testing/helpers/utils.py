03fb2a7d2bb4b9ec6deeef8410e5135db8ed492d
baileyspraggins <baileyspraggins@me.com>
2023-12-16 22:41:58 -0600

    finalize test setup

--- tests/wake_testing/helpers/utils.py
+++ tests/wake_testing/helpers/utils.py
@@ -1,9 +1,17 @@
 
+class Constants():
+    ONE_YEAR = 60 * 60 * 24 * 365
+    ONE_WEEK = 60 * 60 * 24 * 7
+    ONE_DAY = 60 * 60 * 24
+    ONE_HOUR = 60 * 60
+    ONE_MINUTE = 60
+
+    FIXED_POINT_ONE = 10 ** 18
 
 class EvmMath():
     
     def parse_eth(value) -> int:
-        return int(value * 10 ** 18)
+        return int(value * Constants.FIXED_POINT_ONE)
     
     def parse_decimals(value: any, decimals: int) -> int:
         return int(value * 10 ** decimals)
