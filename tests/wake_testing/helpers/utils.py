6e62ffa21115d665c8ecd5e49e2a57b362d556f5
baileyspraggins <baileyspraggins@me.com>
2023-12-18 22:30:22 -0600

    Update utils to account for python float -> int conversion issues

--- tests/wake_testing/helpers/utils.py
+++ tests/wake_testing/helpers/utils.py
@@ -1,17 +1,20 @@
+from decimal import Decimal
 
 class Constants():
-    ONE_YEAR = 60 * 60 * 24 * 365
-    ONE_WEEK = 60 * 60 * 24 * 7
-    ONE_DAY = 60 * 60 * 24
-    ONE_HOUR = 60 * 60
     ONE_MINUTE = 60
+    ONE_HOUR = ONE_MINUTE * 60
+    ONE_DAY = ONE_HOUR * 24
+    ONE_WEEK = ONE_DAY * 7
+    ONE_YEAR = ONE_WEEK * 52
 
     FIXED_POINT_ONE = 10 ** 18
 
 class EvmMath():
     
     def parse_eth(value) -> int:
-        return int(value * Constants.FIXED_POINT_ONE)
+        d = Decimal(value)
+        return int(d * Constants.FIXED_POINT_ONE)
     
     def parse_decimals(value: any, decimals: int) -> int:
-        return int(value * 10 ** decimals)
+        d = Decimal(value)
+        return int(d * 10 ** decimals)
