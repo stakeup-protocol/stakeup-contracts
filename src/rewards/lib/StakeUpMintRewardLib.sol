9ee4e7717d0055feb7ecbac571d9af1d87b0c2c1
Bailey <baileyspraggins@me.com>
2024-07-04 17:26:37 -0500

    SUP reward lib update

--- src/rewards/lib/StakeUpMintRewardLib.sol
+++ src/rewards/lib/StakeUpMintRewardLib.sol
@@ -11,27 +11,11 @@ pragma solidity ^0.8.0;
 library StakeUpMintRewardLib {
     // =================== Mint Reward Cutoffs ===================
 
-    /// @notice Mint reward cutoff for native minting on Ethereum Mainnet | 3% of total SUP supply
-    uint256 private constant MINT_REWARD_CUTOFF_MAINNET = 30_000_000e18;
+    /// @notice Mint reward cutoff for native minting on Ethereum Mainnet | 5% of total SUP supply
+    uint256 private constant MINT_REWARD_CUTOFF_ARBITRUM = 50_000_000e18;
 
-    /// @notice Mint reward cutoff for native minting on Arbitrum | 1.75% of total SUP supply
-    uint256 private constant MINT_REWARD_CUTOFF_ARBITRUM = 17_500_000e18;
-
-    /// @notice Mint reward cutoff for native minting on Polygon POS | 0.75% of total SUP supply
-    uint256 private constant MINT_REWARD_CUTOFF_POLYGON = 7_500_000e18;
-
-    /// @notice Mint reward cutoff for native minting on Base | 0.50% of total SUP supply
-    uint256 private constant MINT_REWARD_CUTOFF_BASE = 5_000_000e18;
-
-    /// @notice Mint reward cutoff for native minting on Sepolia Testnet (This is for testing purposes only)
-    uint256 private constant MINT_REWARD_CUTOFF_SEPOLIA = 100_000_000e18;
-
-    /// @notice Mint reward cutoff for native minting on Plume Testnet (This is for testing purposes only)
-    uint256 private constant MINT_REWARD_CUTOFF_PLUME_TESTNET = 100_000_000e18;
-
-    /// @notice Mint reward cutoff for native minting on Berachain Testnet (This is for testing purposes only)
-    uint256 private constant MINT_REWARD_CUTOFF_BERACHAIN_TESTNET =
-        100_000_000e18;
+    /// @notice Mint reward cutoff for native minting on Arbitrum Sepolia Testnet (This is for testing purposes only)
+    uint256 private constant MINT_REWARD_CUTOFF_ARB_SEPOLIA = 100_000_000e18;
 
     /// @notice Mint reward cutoff for local development (This is for testing purposes only)
     uint256 private constant MINT_REWARD_CUTOFF_LOCAL = 200_000_000e18;
@@ -40,31 +24,15 @@ library StakeUpMintRewardLib {
 
     /// @notice Returns the mint reward allocation for a given stTBY deployment based on the chain ID
     function _getMintRewardAllocation() internal view returns (uint256) {
-        if (block.chainid == 1) {
-            return MINT_REWARD_CUTOFF_MAINNET;
-        }
         if (block.chainid == 42161) {
             return MINT_REWARD_CUTOFF_ARBITRUM;
         }
-        if (block.chainid == 137) {
-            return MINT_REWARD_CUTOFF_POLYGON;
-        }
-        if (block.chainid == 8453) {
-            return MINT_REWARD_CUTOFF_BASE;
-        }
-        if (block.chainid == 11155111) {
-            return MINT_REWARD_CUTOFF_SEPOLIA;
-        }
-        if (block.chainid == 161221135) {
-            return MINT_REWARD_CUTOFF_PLUME_TESTNET;
-        }
-        if (block.chainid == 80085) {
-            return MINT_REWARD_CUTOFF_BERACHAIN_TESTNET;
+        if (block.chainid == 421614) {
+            return MINT_REWARD_CUTOFF_ARB_SEPOLIA;
         }
         if (block.chainid == 31337) {
             return MINT_REWARD_CUTOFF_LOCAL;
         }
-
         return 0;
     }
 }
