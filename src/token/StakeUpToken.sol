e5bb5fa6d947b8a8c24daa140536dc6c7c543d8a
Bailey <baileyspraggins@me.com>
2024-09-19 18:54:52 -0500

    Update StakeUpTokenLite to inherit the OFTController; Fix globalShares bug

--- src/token/StakeUpToken.sol
+++ src/token/StakeUpToken.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import {OFT, ERC20} from "@LayerZero/oft/OFT.sol";
+import {ERC20} from "@LayerZero/oft/OFT.sol";
 import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 import {StakeUpConstants as Constants} from "../helpers/StakeUpConstants.sol";
 import {StakeUpErrors as Errors} from "../helpers/StakeUpErrors.sol";
 
+import {StakeUpTokenLite} from "./StakeUpTokenLite.sol";
+
 import {IStakeUpToken} from "../interfaces/IStakeUpToken.sol";
 import {IStakeUpStaking} from "../interfaces/IStakeUpStaking.sol";
 
-contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
+contract StakeUpToken is IStakeUpToken, StakeUpTokenLite, Ownable2Step {
     // =================== Storage ===================
 
     /// @notice The global supply of the token
@@ -40,8 +42,8 @@ contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
         address gaugeDistributor, // Optional parameter for the gauge distributor
         address owner,
         address layerZeroEndpoint,
-        address layerZeroDelegate
-    ) OFT("StakeUp Token", "SUP", layerZeroEndpoint, layerZeroDelegate) Ownable2Step() {
+        address bridgeOperator
+    ) StakeUpTokenLite(layerZeroEndpoint, bridgeOperator) Ownable2Step() {
         _stakeupStaking = stakeupStaking;
 
         _authorizedMinters[_stakeupStaking] = true;
@@ -60,7 +62,8 @@ contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
      * @param to The address that will receive the tokens
      * @param amount The amount of tokens to mint
      */
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner {
+        _updateGlobalSupply(amount);
         _mint(to, amount);
     }
 
@@ -76,6 +79,7 @@ contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
 
     /// @inheritdoc IStakeUpToken
     function mintRewards(address recipient, uint256 amount) external override onlyAuthorized {
+        _updateGlobalSupply(amount);
         _mint(recipient, amount);
     }
 
@@ -92,12 +96,20 @@ contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
      */
     function _mintAndStartVest(address to, uint256 amount) internal {
         require(to != address(0), Errors.InvalidRecipient());
-        // require(amount > 0, Errors.ZeroAmount);
+        require(amount > 0, Errors.ZeroAmount());
+
+        _updateGlobalSupply(amount);
+        // Set the vesting state for this recipient in the vesting contract & mint tokens
         address stakeupStaking = _stakeupStaking;
-        // Set the vesting state for this recipient in the vesting contract
         IStakeUpStaking(stakeupStaking).vestTokens(to, amount);
-        // Mint the tokens to the vesting contract
         _mint(stakeupStaking, amount);
+    }
+
+    function _updateGlobalSupply(uint256 amount) internal {
+        if (globalSupply() + amount > Constants.MAX_SUPPLY) {
+            revert Errors.ExceedsMaxSupply();
+        }
+        _globalSupply += amount;
     }
 
     function transferOwnership(address newOwner) public override(Ownable, Ownable2Step) {
@@ -106,13 +118,5 @@ contract StakeUpToken is IStakeUpToken, OFT, Ownable2Step {
 
     function _transferOwnership(address newOwner) internal override(Ownable, Ownable2Step) {
         super._transferOwnership(newOwner);
-    }
-
-    function _mint(address account, uint256 amount) internal override(ERC20) {
-        if (globalSupply() + amount > Constants.MAX_SUPPLY) {
-            revert Errors.ExceedsMaxSupply();
-        }
-        _globalSupply += amount;
-        super._mint(account, amount);
     }
 }
