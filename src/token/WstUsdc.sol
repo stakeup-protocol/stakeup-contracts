b58da3e2a33e7041c5b3e142f30d1312906e948a
Bailey <baileyspraggins@me.com>
2024-12-10 06:20:02 -0600

    SUP balance check fix

--- src/token/WstUsdc.sol
+++ src/token/WstUsdc.sol
@@ -53,13 +53,13 @@ contract WstUsdc is IWstUsdc, WstUsdcLite, ERC1155TokenReceiver {
     /// @inheritdoc IWstUsdc
     function depositTby(uint256 tbyId, uint256 amount) external override returns (uint256 amountMinted) {
         _tby.safeTransferFrom(msg.sender, address(this), tbyId, amount, "");
-        amountMinted = _stUsdc.depositTby(tbyId, amount);
 
         uint256 supStartBalance = _sup.balanceOf(address(this));
+        amountMinted = _stUsdc.depositTby(tbyId, amount);
         amountMinted = _mintWstUsdc(amountMinted);
-
-        // If SUP rewards have been minted, transfer to the user
         uint256 supEndBalance = _sup.balanceOf(address(this));
+        
+        // If SUP rewards have been minted, transfer to the user
         if (supEndBalance > supStartBalance) {
             uint256 supMinted = supEndBalance - supStartBalance;
             _sup.safeTransfer(msg.sender, supMinted);
